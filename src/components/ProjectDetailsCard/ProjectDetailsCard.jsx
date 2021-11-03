7bc953aed40b9526c3f63c5b0d0ba3ee842aafdf
unknown <tayebediba25@gmail.com>
2021-11-03 12:09:33 +0330

    update-page-fitsSkill

---
+++ src/components/ProjectDetailsCard/ProjectDetailsCard.jsx
@@ -0,0 +1,11 @@
+import { Card, Grid } from "@material-ui/core";
+import React from "react";
+
+const ProjectDetailsCard = () => {
+  return <Grid container>
+<Card>
+
+</Card>
+  </Grid>;
+};
+export default ProjectDetailsCard;
--- src/pages/FitsSkill/FitsSkill.jsx
+++ src/pages/FitsSkill/FitsSkill.jsx
@@ -1,8 +1,19 @@
-import { Box, Chip, Grid, Slider, TextField } from "@material-ui/core";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Chip,
+  Grid,
+  Slider,
+  TextField,
+  Typography,
+} from "@material-ui/core";
 import { Autocomplete } from "@material-ui/lab";
 import React from "react";
+import ProjectDetailsCard from "../../components/ProjectDetailsCard/ProjectDetailsCard";
 import ChipsArray from "./ChipsArray";
 import "./fitsSkill.css";
+import MoreSkills from "./MoreSkills";
 
 function valuetext(value) {
   return `${value}°C`;
@@ -41,51 +52,99 @@ const FitsSkill = () => {
         autoComplete="off"
         style={{
           display: "flex",
+          flexDirection: "column",
           justifyContent: "space-between",
           margin: "2rem",
         }}
       >
-        <Grid item md={6} xs={12}>
-          <div>
-            <label>مهارت های شما</label>
-            <ChipsArray />
-          </div>
-          <div>
-            <Autocomplete
-              id="combo-box-demo"
-              options={city}
-              getOptionLabel={(option) => option.title}
-              style={{ width: 500, marginTop: "2rem" }}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  label="شهر"
-                  placeholder="انتخاب کنید"
-                  variant="outlined"
-                  style={{
-                    backgroundColor: "#fff",
-                    width: "100%",
-                    margintop: "3rem",
-                  }}
+        <Grid container>
+          <Grid item md={6} xs={12}>
+            <div>
+              <label>مهارت های شما</label>
+              <ChipsArray />
+            </div>
+            <Grid container>
+              <Grid item md={6} xs={12}>
+                <Autocomplete
+                  id="combo-box-demo"
+                  options={city}
+                  getOptionLabel={(option) => option.title}
+                  style={{ width: 250, marginTop: "2rem" }}
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      label="شهر"
+                      placeholder="انتخاب کنید"
+                      variant="outlined"
+                      style={{
+                        backgroundColor: "#fff",
+                        width: "100%",
+                        margintop: "3rem",
+                      }}
+                    />
+                  )}
                 />
-              )}
-            />
-          </div>
-          <div  style={{ width: 500, marginTop: "2rem" }}>
-            <label>قیمت</label>
-            <Box sx={{ width: "100%", marginTop: "1.5rem" }}>
-              <Slider
-                getAriaLabel={() => "Temperature range"}
-                value={value}
-                onChange={handleChange}
-                valueLabelDisplay="auto"
-                getAriaValueText={valuetext}
-                marks={marks}
-              />
-            </Box>
-          </div>
+              </Grid>
+              <Grid item md={6} xs={12} style={{ marginTop: "2rem" }}>
+                <label>نوع جستوجو</label>
+                <ButtonGroup
+                  color="primary"
+                  aria-label="contained  primary button group"
+                >
+                  <Button>همه</Button>
+                  <Button>پروژه</Button>
+                  <Button>مسابقه</Button>
+                </ButtonGroup>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item md={6} xs={12}>
+            <div>
+              <MoreSkills />
+            </div>
+            <div style={{ width: 500, marginTop: "2rem", padding: "1rem" }}>
+              <label>قیمت</label>
+              <Box sx={{ width: "100%", marginTop: "1.5rem" }}>
+                <Slider
+                  getAriaLabel={() => "Temperature range"}
+                  value={value}
+                  onChange={handleChange}
+                  valueLabelDisplay="auto"
+                  getAriaValueText={valuetext}
+                  marks={marks}
+                />
+              </Box>
+            </div>
+          </Grid>
+        </Grid>
+        <Grid container>
+          <Grid item md={6} xs={12}>
+            <Typography variant="inherit" color="textSecondary">
+              50 پروژه پیدا شد
+            </Typography>
+            <Typography
+              variant="button"
+              color="primary"
+              style={{ fontWeight: "bold" }}
+            >
+              پاک کردن فیلترها
+            </Typography>
+          </Grid>
+          <Grid item md={6} xs={12} style={{ textAlign: "left" }}>
+            <Button className="btnFits" variant="contained">
+              اعمال فیلترها
+            </Button>
+          </Grid>
         </Grid>
       </form>
+
+      <Grid>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((item) => (
+          <Grid style={{ padding: "0 2rem" }} item md={4} xs={12} key={item}>
+            <ProjectDetailsCard />
+          </Grid>
+        ))}
+      </Grid>
     </Grid>
   );
 };
