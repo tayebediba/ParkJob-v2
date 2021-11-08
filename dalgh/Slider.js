b6dd56cde127df16e80c0ad2caeb69c0263cd859
unknown <tayebediba25@gmail.com>
2021-11-08 10:54:36 +0330

    fixed-npm- redux

--- dalgh/Slider.js
+++ dalgh/Slider.js
@@ -59,6 +59,7 @@ const Slider = () => {
         }}
         className={classes.swiperContainer}
       >
+         <TopFreelancersCard data={item} key={item.id} />
         {commentCardData.map(({ image, username, job, comment }) => (
           <SwiperSlide key={username}>
             {({ isActive }) => (
